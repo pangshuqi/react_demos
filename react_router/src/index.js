3cb14f0927c7474b717c919bcd008fa88c9a0cf8
HanxgSir <18304018283@163.com>
2016-12-28 13:55:41 +0800

    react,react-router

--- react_router/src/index.js
+++ react_router/src/index.js
@@ -20,7 +20,6 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Nav />
                 {this.props.children}
             </div>
         )
