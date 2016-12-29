3c5ed54afdc382a2ccc7800137ab8a1673ad5184
HanxgSir <18304018283@163.com>
2016-12-29 13:27:38 +0800

    react,react-router，test localStorage

--- react_router/components/home/home.js
+++ react_router/components/home/home.js
@@ -6,14 +6,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from '../nav/nav'
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+    }
+
     render() {
         return (
             <div>
                 <Nav />
                 <h2 style={{textAlign:'center'}}>这是home</h2>
+                <input type="text" placeholder="请输入用户名" ref="username"/>
+                <input type="password" placeholder="请输入用密码" ref="password"/>
+                <button onClick={this.login}>signIn</button>
             </div>
 
         );
+    }
+
+    login() {
+        let params = {
+            username: this.refs.username.value,
+            password: this.refs.password.value
+        };
+        $.post('../data/user.json', params, function (data) {
+            console.log(data);
+            if (params.username == data.users[0].username && params.password == data.users[0].password) {
+                console.log('success');
+                let token = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+                localStorage.token= 'token';
+            }
+        }.bind(this))
     }
 }
 
--- react_router/components/nav/navli.js
+++ react_router/components/nav/navli.js
@@ -5,13 +5,17 @@
 import React from 'react';
 import {Link} from 'react-router';
 class NavLi extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         let linkTo = this.props.name == "Home" ? "/" : "/" + this.props.name;
         let activeClass = this.props.name == "Home" ? "" : "active";
         let num = (new Date() + Math.floor(Math.random() * 999999)).toString(36);
         return (
             <li key={num}>
-                <Link to={linkTo} activeClassName={activeClass}>
+                <Link to={linkTo} activeClassName={activeClass} >
                     {this.props.name}
                 </Link>
             </li>
