a8dca2933c67e27c1da7570253a8ea78586effe8
HanxgSir <18304018283@163.com>
2017-01-22 17:18:08 +0800

    city selector

---
+++ react-components/webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var webpack = require('webpack');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+module.exports = {
+    entry: {
+        'city': ['./city/example.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'static'),
+        filename: '[name].js',
+        chunkFilename: "[id].chunk.js",
+        publicPath: "/static"
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            jQuery: "jquery",
+            $: "jquery"
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ["style", "css"]
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: "url-loader?limit=102400&name=/images/[hash:8].[ext]"
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
